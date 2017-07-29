9476700958c01335ad9e6296ccafe9f2a4af0df8
beimagine <rafal.wywrot@onet.pl>
2017-07-29 15:41:58 +0200

    dodanie funkcji sortowanie

--- Scripts.js
+++ Scripts.js
@@ -20,3 +20,20 @@ Favoruite_color.addEventListener("click", function () {
 function zmiana(name_column) {
     alert("Kolumna : " + name_column);
 }
+//
+// function tablica() {
+//     this.width = 150px;
+//     this.height = 25px;
+//     this.fontsize(20);
+//     this.color="black";
+//     this.textAlign = center;
+// }
+var my_table = document.getElementById('TableContainer');
+var rows_table = my_table.getElementsByTagName('tr');
+
+function sortowanie(number ,name, surname ,color) {
+    this.number = number;
+    this.name = name;
+    this.surname = surname ;
+    this.color = color;
+}
