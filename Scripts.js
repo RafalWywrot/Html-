8d6337d6819d2efd1f7b20ad886bb5a184aef59c
beimagine <rafal.wywrot@onet.pl>
2017-08-23 21:55:11 +0200

    add new function, compareValues

--- Scripts.js
+++ Scripts.js
@@ -4,19 +4,26 @@ var Surname = document.getElementById("Surname");
 var Favoruite_color = document.getElementById("Color");
 
 Number.addEventListener("click", function () {
-    change_table(1);
+    new_table.sort(compareValues('number')),
+    changeOrderInTable()
 }, true);
+
 Name.addEventListener("click", function () {
-    change_table(2);
+    new_table.sort(compareValues('name')),
+    changeOrderInTable()
 }, true);
+
 Surname.addEventListener("click", function () {
-    change_tablea(3);
+    new_table.sort(compareValues('surname')),
+    changeOrderInTable()
 }, true);
+
 Favoruite_color.addEventListener("click", function () {
-    change_table(4);
+    new_table.sort(compareValues('color')),
+    changeOrderInTable()
 }, true);
 
-
+//we create a object which will represent every row in table
 var Person = function (number, name, surname, color) {
     this.number = number;
     this.name = name;
@@ -28,73 +35,40 @@ var my_table = document.getElementById('TableContainer');
 var rows_table = my_table.getElementsByTagName('tbody')[0].getElementsByTagName('tr');
 var new_table = [];
 
+
 for (var i = 0, length = rows_table.length; i < length; i++) {
     var tds = rows_table[i].getElementsByTagName('td');
     new_table.push(new Person(tds[0].innerText, tds[1].innerText, tds[2].innerText, tds[3].innerText));
 }
 
-// my_table.getElementsByTagName('tbody')[0].insertBefore(rows_table[4], rows_table[1]);
+function compareValues(key) {
+    return function (a, b) {
+    // console.log( a["name"]);
+    // console.log(b);
+        if (!a.hasOwnProperty(key) || !b.hasOwnProperty(key)) {
+        //we must check if object has property named in key to be able to compare values
+            return 0;
+        }
 
-function change_table(number) {
-
-    switch (number) {
-        case 1:
-            new_table.sort(function (ob1, ob2) {
-                if (ob1.number > ob2.number) {
-                    return 1;
-                } else if (ob1.number < ob2.number) {
-                    return -1;
-                }
-                else {
-                    return 0;
-                }
-            });
-            break;
-
-        case 2:
-            new_table.sort(function (ob1, ob2) {
-                if (ob1.name > ob2.name) {
-                    return 1;
-                } else if (ob1.name < ob2.name) {
-                    return -1;
-                }
-                else {
-                    return 0;
-                }
-            });
-            break;
-
-        case 3:
-            new_table.sort(function (ob1, ob2) {
-                if (ob1.surname > ob2.surname) {
-                    return 1;
-                } else if (ob1.surname < ob2.surname) {
-                    return -1;
-                }
-                else {
-                    return 0;
-                }
-            });
-            break;
-
-        case 4:
-            new_table.sort(function (ob1, ob2) {
-                if (ob1.color > ob2.color) {
-                    return 1;
-                } else if (ob1.color < ob2.color) {
-                    return -1;
-                }
-                else {
-                    return 0;
-                }
-            });
-            break;
-
-        default:
-            console.log("bład w petli switch");
-            break;
+        const varA = (typeof a[key] === 'string') ?
+            a[key].toUpperCase() : a[key];
+        const varB = (typeof b[key] === 'string') ?
+            b[key].toUpperCase() : b[key];
+        // now we check, if first value is bigger than second function change them in ascending order,
+        // , if is less then function return -1, and change them in descending order
+        //if the values are the same function return 0 and nothing is change
+        if (varA > varB) {
+            return 1;
+        } else if (varA < varB) {
+            return -1;
+        } else {
+            return 0;
+        }
     }
-    for (var i = 0; i < 8; i++) {
+}
+
+ function changeOrderInTable() {
+     for (var i = 0; i < 8; i++) {
         rows_table[i].getElementsByTagName('td')[0].innerHTML = new_table[i].number;
         rows_table[i].getElementsByTagName('td')[1].innerHTML = new_table[i].name;
         rows_table[i].getElementsByTagName('td')[2].innerHTML = new_table[i].surname;
@@ -102,5 +76,4 @@ function change_table(number) {
     }
 }
 
-console.log(new_table);
 
